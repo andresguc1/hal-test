d41d70353da9ba56e0626e4ef5e8a954bc5f88bf
andresguc <andresguc@gmail.com>
2026-04-10 14:26:09 -0500

    chore: update frontend assets and stylesheet references in index.html

--- apps/backend/public/app/assets/CDWEKZTF-CmuiLfI8.js
+++ apps/backend/public/app/assets/CDWEKZTF-CmuiLfI8.js
@@ -49,7 +49,7 @@ import {
     U as Os,
     V as qs,
     W as _s,
-} from './index-BFoxQNRn.js';
+} from './index-B9vFRcl_.js';
 var Rs = (e) => e != null,
     zs = (e) => e.filter(Rs);
 function Ks(e) {
